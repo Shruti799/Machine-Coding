ac12d9f04e501b5edebb764cdea7ac17fb83d42c
Shruti799 <shrutijwal999@gmail.com>
2026-04-16 00:11:19 +0530

    modified and added packages

--- TicTacToe_LLD/PlayGame.java
+++ TicTacToe_LLD/PlayGame.java
@@ -1,5 +1,22 @@
 package TicTacToe_LLD;
+import TicTacToe_LLD.models.*;
 
 public class PlayGame {
-    
+    public void main(String[] args){
+        System.out.println("Let's play TicTacToe Game");
+        TicTacToeGame game = new TicTacToeGame();
+        game.initializeGame();
+        GameStatus status = game.startGame();
+        switch(status){
+            case WIN:
+                System.out.println(game.winner.name + "won game.");
+                break;
+            case DRAW:
+                System.out.println("Game drawn");
+                break;
+            default:
+                System.out.println("No result");
+                break;
+        }
+    }
 }
--- TicTacToe_LLD/TicTacToeGame.java
+++ TicTacToe_LLD/TicTacToeGame.java
@@ -2,10 +2,14 @@ package TicTacToe_LLD;
 import TicTacToe_LLD.models.*;
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 
 public class TicTacToeGame {
     Deque<Player> players;
     Board gameBoard;
+    Player winner;
 
     public void initializeGame(){
         players = new LinkedList<>();
@@ -57,5 +61,46 @@ public class TicTacToeGame {
         }
 
         return (colCheck || rowCheck || diagonalCheck || antiDiagonalCheck);
+    }
+
+    public GameStatus startGame(){
+        boolean isWinner = false;
+        while(!isWinner){
+            List<Pair<Integer, Integer>> emptyCells = gameBoard.getEmptyCells();
+
+            Player currentPlayer = players.removeFirst();
+
+            if(emptyCells.isEmpty()){
+                isWinner = true;
+                continue;
+            }
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("currentPlayer.name" + "," + "Enter row");
+            int row = sc.nextInt();
+
+            System.out.println("currentPlayer.name" + "," + "Enter col");
+            int col = sc.nextInt();
+
+            boolean validMove = gameBoard.addPiece(row, col, currentPlayer.playingPiece.pieceType);
+
+            if(!validMove){
+                System.out.println("Not a valid move. Enter the row and column again.");
+                players.addFirst(currentPlayer);
+                continue;
+            }
+
+            isWinner = checkWinner(row, col, currentPlayer.playingPiece.pieceType);
+
+            if(isWinner){
+                gameBoard.printBoard();
+                winner = currentPlayer;
+                return GameStatus.WIN;
+            }
+
+            players.addLast(currentPlayer);
+        }
+        return GameStatus.DRAW;
     }
 }
