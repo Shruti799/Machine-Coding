6d14da4d5999ebd10664738c2dab16cdfcc9f7fc
Shruti799 <shrutijwal999@gmail.com>
2026-04-26 18:16:29 +0530

    added

--- Elevator_LLD/ElevatorCar.java
+++ Elevator_LLD/ElevatorCar.java
@@ -1,14 +1,72 @@
 package Elevator_LLD;
 
+import Elevator_LLD.enums.ElevatorDirection;
+
 public class ElevatorCar {
 
     private final String id;
-    private final ElevatorController controller;
+    public int currentFloor;
+    public int nextStoppage;
+    Door door;
+    ElevatorDirection movingDirection;
+   
 
-    public ElevatorCar(String id, ElevatorController controller){
+    public ElevatorCar(String id){
         this.id = id;
-        this.controller = controller;
+        currentFloor = 0;
+        door = new Door();
+        movingDirection = ElevatorDirection.IDLE;
     }
 
-    
+    public void setCurrentFloor(int currentFloor){
+        this.currentFloor = currentFloor;
+    }
+
+    public void showDisplay(){
+        System.out.println("Current Floor: " + currentFloor + " " + "Direction: " + movingDirection);
+    }
+
+    public void moveElevatorCar(int destinationFloor){
+
+        this.nextStoppage = destinationFloor;
+
+        if(nextStoppage == currentFloor){
+            showDisplay();
+            door.openDoor(id);
+            return;
+        }
+
+        int startFloor = currentFloor;
+        door.closeDoor(id);
+
+        if(nextStoppage > currentFloor){
+            movingDirectionirection = ElevatorDirection.UP;
+            showDisplay();
+            for(int i=startFloor+1;i<=nextStoppage;i++){
+                try {
+                    Thread.sleep(6);
+                } catch (Exception e) {
+                    // TODO: handle exception
+                }
+                setCurrentFloor(i);
+                showDisplay();
+            }
+        }
+        else{
+            movingDirectionirection = ElevatorDirection.DOWN;
+            showDisplay();
+            for(int i=startFloor-1;i>=nextStoppage;i--){
+                try {
+                    Thread.sleep(6);
+                } catch (Exception e) {
+                    // TODO: handle exception
+                }
+                setCurrentFloor(i);
+                showDisplay();
+            }
+        }
+        door.openDoor(id);
+    }
+
+
 }
--- Elevator_LLD/ElevatorController.java
+++ Elevator_LLD/ElevatorController.java
@@ -1,7 +1,45 @@
 package Elevator_LLD;
 
+import java.util.concurrent.PriorityBlockingQueue;
+
 public class ElevatorController {
-    
-    
+
+    private ElevatorCar elevatorCar;
+    PriorityBlockingQueue<Integer> upMinQ;
+    PriorityBlockingQueue<Integer> downMaxQ;
+
+    public ElevatorController(ElevatorCar elevatorCar){
+        this.elevatorCar = elevatorCar;
+        upMinQ = new PriorityBlockingQueue<>();
+        downMaxQ = new PriorityBlockingQueue<>(10, (a,b) -> b-a);
+    }
+
+    public void submitRequest(int destinationFloor){
+        enqueue(destinationFloor);
+    }
+
+    public void enqueue(int destinationFloor){
+
+        int nextStop = elevatorCar.nextStoppage;
+
+        if(destinationFloor == nextStop){
+            return;
+        }
+
+        else if(destinationFloor > nextStop){
+           if(!upMinQ.contains(destinationFloor)){
+             upMinQ.offer(destinationFloor);
+           }
+        }
+
+        else{
+            if(!downMaxQ.contains(destinationFloor)){
+              downMaxQ.offer(destinationFloor);
+            }
+        }
+    }
+
+
+
 
 }
